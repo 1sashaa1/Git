549c53316aadf300d16a0c2bb6730b80616b1859
Polina <polinausolceva44@gmai.com>
2024-09-27 16:16:38 +0300

    commit_1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "This file was changed!" );
+        System.out.println( "lalalal" );
+
     }
 }
